65ffddb65bcca03fdbc36f36dbae10f5efdd8a54
NicoDayo <josephchoisong@gmail.com>
2026-01-27 21:31:52 -0800

    Added Lab 2

---
+++ L2_PortScanner/scanner.py
@@ -0,0 +1,41 @@
+import nmap
+nMap = nmap.PortScanner()
+target_host="127.0.0.1"
+ports="20-49151"
+
+try: #running the scan once to see if any errors occur or nmap doesn't work
+    nMap.scan(target_host, ports, timeout=400)
+except nmap.PortScannerError as error:
+    print(f"ERROR: {error}")
+    raise SystemExit(0)
+
+#scan information and command
+print("===========================")
+print(f"command - {nMap.command_line()}")
+print(f"scan information: {nMap.scaninfo()}")
+print("===========================")
+
+nMap.all_hosts()
+if not nMap.all_hosts: #if any hosts are not reachable then throws an error
+    print(f"ERROR: No host found: {target_host} unreachable or scan has failed")
+    raise SystemExit(0)
+
+for host in nMap.all_hosts():
+
+    print("==========================")
+    print(f"hosts: {nMap[host].hostname()}")
+    print(f"status: {nMap[host].state()}")
+    for ptcl in nMap[host].all_protocols():
+        print("======================")
+        print(f"Protocol > {ptcl}")
+        port_list = list(nMap[host][ptcl])
+        port_list.sort()
+
+        for port in port_list:
+            content = nMap[host][ptcl][port]
+            state = content.get("state", "unknown")
+            service = content.get("name", "")
+            if service:
+                print(f"port: {port} : {state} | {service}")
+            else:
+                print(f"port: {port} : {state}")
---
+++ L2_PortScanner/smallerscan.py
@@ -0,0 +1,39 @@
+import nmap
+nMap = nmap.PortScanner()
+target_host="127.0.0.1"
+ports="20-2236"
+
+try: #running the scan once to see if any errors occur or nmap doesn't work
+    nMap.scan(target_host, ports, timeout=400)
+except nmap.PortScannerError as error:
+    print(f"ERROR: {error}")
+    raise SystemExit(0)
+
+#scan information and command
+print(f"command - {nMap.command_line()}")
+print(f"scan information: {nMap.scaninfo()}\n")
+
+nMap.all_hosts()
+if not nMap.all_hosts: #if any hosts are not reachable then throws an error
+    print(f"ERROR: No host found: {target_host} unreachable or scan has failed")
+    raise SystemExit(0)
+
+for host in nMap.all_hosts():
+
+    print("======================")
+    print(f"hosts: {nMap[host].hostname()}")
+    print(f"status: {nMap[host].state()}")
+    print(f"Protocol > {ptcl}")
+    print("======================")
+    for ptcl in nMap[host].all_protocols():
+        port_list = list(nMap[host][ptcl])
+        port_list.sort()
+
+        for port in port_list:
+            content = nMap[host][ptcl][port]
+            state = content.get("state", "unknown")
+            service = content.get("name", "")
+            if service:
+                print(f"port: {port} : {state} | {service}")
+            else:
+                print(f"port: {port} : {state}")
